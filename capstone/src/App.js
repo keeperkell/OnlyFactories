8df99d53fee657fb71a2bff5b5728b1844a93e4d
keeperkell <keeperkell@gmail.com>
2021-10-11 18:53:39 -0700

    AWS ID pool

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -7,6 +7,13 @@ import About from './webpages/about';
 import Ordering from './webpages/ordering'
 import Tracking from './webpages/tracking'
 
+var AWS = require('aws-sdk');
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-2:b854dcaf-5a7e-4052-8d4f-8db0cd8a977f',
+});
+
 function App() {
   return (
     <Router>
