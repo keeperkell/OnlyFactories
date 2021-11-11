82d7793d5f8e25b1ad7de53670dfc7ead19d07ec
keeperkell <keeperkell@gmail.com>
2021-11-11 14:36:00 -0800

    override MUI primary,secondary,error color options

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -15,7 +15,24 @@ import { IconButton } from '@material-ui/core';
 import PlayArrowIcon  from '@material-ui/icons/PlayArrow';
 import FavoriteIcon  from '@material-ui/icons/Favorite';
 
+import {ThemeProvider, createTheme} from '@mui/material/styles'
+
 Amplify.configure(awsconfig);
+
+// color override for MUI theme, unable to use css vars here
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f1b300', //primaryPrideGold in globalStyle.css
+    },
+    secondary: {
+      main: '#808080', //primarySilver in globalStyle.css
+    }, 
+    error: {
+      main: '#d22630', //accentGarnet in globalStyle.css
+    },
+  },
+});
 
 function App() {
     
@@ -37,41 +54,43 @@ function App() {
   }
 
   return (
-    <div className='App'>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
 
-      <AmplifySignOut />
+        <AmplifySignOut />
 
-      <Router>
-        <Header />
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/ordering' component={Ordering} />
-            <Route path='/tracking' component={Tracking} />
-            <Route path='/about' component={About} />
+        <Router>
+          <Header />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/ordering' component={Ordering} />
+              <Route path='/tracking' component={Tracking} />
+              <Route path='/about' component={About} />
 
-          </Switch>
-      </Router>
+            </Switch>
+        </Router>
 
-      <div className="orderList">
-        { orders.map(order => {
-          return (
-            <Paper variant="outlined" elevation={2}>
-              <div className="orderCard">
-                <IconButton aria-label="play">
-                  <PlayArrowIcon />
-                </IconButton>
-                <div>
-                  <div className="OrderID"> {order.OrderID}</div>
-                  <div className="Email">{order.Email}</div>
-                  <div className="Name">{order.Name}</div>
+        <div className="orderList">
+          { orders.map(order => {
+            return (
+              <Paper variant="outlined" elevation={2}>
+                <div className="orderCard">
+                  <IconButton aria-label="play">
+                    <PlayArrowIcon />
+                  </IconButton>
+                  <div>
+                    <div className="OrderID"> {order.OrderID}</div>
+                    <div className="Email">{order.Email}</div>
+                    <div className="Name">{order.Name}</div>
+                  </div>
                 </div>
-              </div>
-            </Paper>
-          )
-        })}
-      </div>
+              </Paper>
+            )
+          })}
+        </div>
 
-    </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
