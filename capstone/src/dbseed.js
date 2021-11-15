53770211007d17004fd4eb9378e4fd68c2b839ed
keeperkell <keeperkell@gmail.com>
2021-11-15 00:50:11 -0800

    updated dbseed.js details

--- capstone/src/dbseed.js
+++ capstone/src/dbseed.js
@@ -1,9 +1,10 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host      : 'factory-orders.ctu961ikr6c4.us-east-2.rds.amazonaws.com',
+  host      : 'onlyfactories.ctu961ikr6c4.us-east-2.rds.amazonaws.com',
   user      : 'admin',
   password  : 'x3QxHJ0IYbxjhY88zQSm',
-  port      : 3306
+  port      : 3306,
+  timeout   : 6000
 })
 
 connection.connect(function(err) {
