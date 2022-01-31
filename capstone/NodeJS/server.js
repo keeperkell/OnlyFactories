17aa18dbf3e0a185c7f1602b1e8aa3eb8cda2cff
jmharris7755 <43102944+jmharris7755@users.noreply.github.com>
2022-01-30 23:17:04 -0800

    API Test
    Testing different setting for API. Commented out lines 22 - 27.. Added to line 6 in server.js

--- capstone/NodeJS/server.js
+++ capstone/NodeJS/server.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require("cors");
 const connection = require("./models/db.js");
 
-const app = express();
+const app = express().use('*', cors());
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
+//var corsOptions = {
+ // origin: "http://localhost:3000"
+//};
 
-var orderID = 1;
+//var orderID = 1;
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json -- middleware
 app.use(express.json());
@@ -19,12 +19,12 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Upgrade-Insecure-Requests");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
-});
+});*/
 
 // simple route
 app.get("/", (req, res) => {
